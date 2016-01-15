04752879d3297942cffa81f76a150453f73c3458
Genesis Lima <genesislima85@gmail.com>
2016-01-15 15:20:32 -0300

    Primeiro commit do projeto e-acervo do NTVRU

---
+++ src/main/java/org/ntvru/eacervo/controller/EmployeesController.java
@@ -0,0 +1,10 @@
+package org.ntvru.eacervo.controller;
+
+import org.springframework.stereotype.Controller;
+
+//Classe que representa os Servidores da instituição
+@Controller
+public class EmployeesController {
+
+	
+}
---
+++ src/main/java/org/ntvru/eacervo/controller/HomeController.java
@@ -0,0 +1,40 @@
+package org.ntvru.eacervo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+   
+	@RequestMapping("/")
+	public String index(){
+		System.out.println("Página principal!");		
+		return "index";		
+	}
+	@RequestMapping("/servidor")
+	public String getEmployeeRegisterPage(){
+		return "servidores/form_servidor";		
+	}
+	@RequestMapping("/produto")
+	public String getProductRegisterPage(){
+		return "produtos/form_produto";
+	}
+	@RequestMapping("/area")
+	public String getAreaRegisterPage(){
+		return "areas/form_area";
+	}
+	@RequestMapping("/tema")
+	public String getTopicRegisterPage(){
+		return "temas/form_tema";		
+	}
+	@RequestMapping("/tipomidia")
+	public String getMediaRegisterPage(){
+		return	"tiposmidia/form_tipomidia";	
+	}
+	
+	@RequestMapping("/procedimento")
+	public String getProcedureRegisterPage(){
+		return "procedimentos/form_procedimento";
+	}
+}
